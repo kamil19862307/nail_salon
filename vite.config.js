a903315cf54a77fc41bece522f4e5a66ce3b153f
kamil <kamil19862307@gmail.com>
2025-01-17 13:40:15 +0300

    First init

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/bootstrap.css',
+                'resources/css/font-awesome.css',
+                'resources/css/basic.css',
+                'resources/css/custom.css',
+
+                'resources/js/jquery-1.10.2.js',
+                'resources/js/bootstrap.js',
+                'resources/js/jquery.metisMenu.js',
+                'resources/js/custom.js',
+            ],
+            refresh: true,
+        }),
+    ],
+});
